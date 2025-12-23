185d544e567f3462cc1f0f4237e2cdf1aa7e09a0
devpark435 <devpark435@gmail.com>
2025-12-23 15:52:53 +0900

    chore: 프로젝트 초기 설정 (Storyboard 제거 및 gitignore 추가)

---
+++ ieum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ieum
+//
+//  Created by 박현렬 on 12/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
